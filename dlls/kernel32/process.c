e4f7ec23ebd18b06ccf6721a2d40d92ec73fa6bd
Brendan McGrath <brendan@redmandi.com>
2019-02-26 19:39:25 +1100

    kernel32: Allow double quote style escape in argv.
    Two quotes together (within outer quotes) represents a single
    quote (with the first quote acting as an escape character)
    
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=46721
    Signed-off-by: Brendan McGrath <brendan@redmandi.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/kernel32/process.c
+++ dlls/kernel32/process.c
@@ -1337,9 +1337,13 @@ static char **build_argv( const WCHAR *cmdlineW, int reserved )
             /* '\', count them */
             bcount++;
         } else if ((*s=='"') && ((bcount & 1)==0)) {
-            /* unescaped '"' */
-            in_quotes=!in_quotes;
-            bcount=0;
+            if (in_quotes && s[1] == '"') {
+               s++;
+            } else {
+               /* unescaped '"' */
+               in_quotes=!in_quotes;
+               bcount=0;
+            }
         } else {
             /* a regular character */
             bcount=0;
@@ -1383,7 +1387,12 @@ static char **build_argv( const WCHAR *cmdlineW, int reserved )
                  */
                 d-=bcount/2;
                 s++;
-                in_quotes=!in_quotes;
+                if(in_quotes && *s == '"') {
+                  *d++='"';
+                  s++;
+                } else {
+                  in_quotes=!in_quotes;
+                }
             } else {
                 /* Preceded by an odd number of '\', this is half that
                  * number of '\' followed by a '"'
